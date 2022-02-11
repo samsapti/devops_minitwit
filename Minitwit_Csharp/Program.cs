68b0125a3f1e07dc2d672081c7585b19fbec3fb1
nissemand243 <xander.s234@gmail.com>
2022-02-11 10:57:19 +0100

    Progress from liveshare

--- Minitwit_Csharp/Program.cs
+++ Minitwit_Csharp/Program.cs
@@ -1,6 +1,73 @@
+using Microsoft.Data.Sqlite;
+using Microsoft.AspNetCore.Mvc;
+
+var dbPath = @"./tmp/minitwit.db";
+var perPage = 30;
+var debug = true;
+var secretKey = "development key";
+var sql = File.ReadAllText("./schema.sql");
+
+
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
+
+
+// open connection to db
+SqliteConnection connectDb()
+{
+    return new SqliteConnection("Filename={dbPath}");
+}
+
+// create db tables
+void initDb(SqliteConnection connection)
+{
+    try
+    {
+        using(connection)
+        {
+            using (SqliteCommand sqlReader = connection.CreateCommand())
+            {
+                connection.Open();
+                sqlReader.CommandText = sql;
+                sqlReader.ExecuteNonQuery();
+            }
+        }
+    }
+    finally
+    {
+        connection.Close();
+    }
+}
+
+
+void queryDb(string query, string[] args, bool one = false)
+{
+    
+}
+
+void getUserId(string username)
+{
+    
+}
+
+void format_datetime(DateTime timestamp)
+{
+    
+}
+
+void gravatar_url(string username)
+{
+    
+}
+
+
+
+
+
+
+
