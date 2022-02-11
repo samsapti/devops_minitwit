4168d49b06d82d113ae3b9bfb5c78fe93440c787
MaiBlom <mai.blommen@gmail.com>
2022-02-11 14:33:36 +0100

    Progress
    Co-authored-by: Mardesol <Mardesol@users.noreply.github.com>
    Co-authored-by: joac1144 <joac1144@users.noreply.github.com>

--- Minitwit_Csharp/Program.cs
+++ Minitwit_Csharp/Program.cs
@@ -1,73 +1,113 @@
 using Microsoft.Data.Sqlite;
-using Microsoft.AspNetCore.Mvc;
 
-var dbPath = @"./tmp/minitwit.db";
-var perPage = 30;
-var debug = true;
-var secretKey = "development key";
-var sql = File.ReadAllText("./schema.sql");
-
-
-
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
-
-app.MapGet("/", () => "Hello World!");
-
-app.Run();
-
-
-// open connection to db
-SqliteConnection connectDb()
+namespace Minitwit;
+public class Program 
 {
-    return new SqliteConnection("Filename={dbPath}");
-}
-
-// create db tables
-void initDb(SqliteConnection connection)
-{
-    try
+    static void Main(string[] args)
     {
-        using(connection)
+        var dbPath = @"./tmp/minitwit.db";
+        var perPage = 30;
+        var debug = true;
+        var secretKey = "development key";
+
+
+        var builder = WebApplication.CreateBuilder(args);
+        var app = builder.Build();
+
+        app.Run();
+
+
+        // open connection to db
+        SqliteConnection connectDb()
         {
-            using (SqliteCommand sqlReader = connection.CreateCommand())
+            return new SqliteConnection($"Data Source={dbPath}");
+        }
+
+        // create db tables
+        void initDb(SqliteConnection connection)
+        {
+            try
+            {
+                using(connection)
+                {
+                    using (SqliteCommand sqlReader = connection.CreateCommand())
+                    {
+                        connection.Open();
+                        sqlReader.CommandText = File.ReadAllText("./schema.sql");
+                        sqlReader.ExecuteNonQuery();
+                    }
+                }
+            }
+            finally
+            {
+                connection.Close();
+            }   
+        }
+
+        void initDb2()
+        {
+            using (SqliteConnection connection = connectDb())
             {
                 connection.Open();
-                sqlReader.CommandText = sql;
+                SqliteCommand sqlReader = connection.CreateCommand();
+                sqlReader.CommandText = File.ReadAllText("./schema.sql");
                 sqlReader.ExecuteNonQuery();
             }
         }
     }
-    finally
+
+    object? queryDb(string query, string[] args, bool one = false)
     {
-        connection.Close();
+        using (SqliteConnection connection = connectDb())
+        {
+            connection.Open();
+            SqliteCommand cmd = connection.CreateCommand();
+            cmd.CommandText = query;
+            
+            if(one)
+            {
+                return cmd.ExecuteScalar();
+            }
+            else
+            {
+                
+            }
+            
+        }
     }
-}
 
 
-void queryDb(string query, string[] args, bool one = false)
-{
+    int getUserId(string username)
+    {
+        using (SqliteConnection connection = connectDb())
+        {
+            connection.Open();
+            SqliteCommand cmd = connection.CreateCommand();
+            cmd.CommandText = "select user_id from user where username = '?'";
+            cmd.Parameters.AddWithValue("?", username);
+            
+            Int32 rv = Convert.ToInt32(cmd.ExecuteScalar());
+
+            return rv;
+        }
+    }
+
+    public string format_datetime(DateTime timestamp)
+    {
+        var formatted_datetime = timestamp
+
+        return formatted_datetime;
+    }
+
+    public void gravatar_url(string username)
+    {
+        
+    }
+
+
+
+
+
     
+
 }
-
-void getUserId(string username)
-{
-    
-}
-
-void format_datetime(DateTime timestamp)
-{
-    
-}
-
-void gravatar_url(string username)
-{
-    
-}
-
-
-
-
-
-
-
